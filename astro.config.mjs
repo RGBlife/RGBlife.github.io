25ee1865b88645cd4665b3dc85ca508fa723b192
Bret <bret_gomes811@hotmail.co.uk>
2023-12-12 22:39:04 +0000

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://RGBlife.github.io',
-  base: '/',
+  base: '/portfolio/',
 });
