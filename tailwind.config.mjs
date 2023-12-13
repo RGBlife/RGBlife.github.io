6d6b1f7973044603ada1fc55e9ecbf63cb42dc42
Bret <bret_gomes811@hotmail.co.uk>
2023-12-13 12:45:50 +0000

    basic styling applied to first section & content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,24 @@ export default {
         inter: ["Inter", "sans-serif"],
       },
       colors: {
-        customSlate: "#a8b2d1",
-      }
+        darkBackground: "#0a192f",
+        darkPrimaryText: "#a8b2d1",
+        darkSecondaryText: "#ccd6f6",
+        darkAccent: "#FF6B6B",
+        darkSecondaryBackground: "#233554",
+        darkTertiaryBackground: "#8892b0",
+        darkHighlight: "#e6f1ff",
+
+        lightBackground: "#ccd6f6",
+        lightPrimaryText: "#0a192f",
+        lightSecondaryText: "#112240",
+        lightAccentTeal: "#36C1B4",
+        lightSecondaryBackground: "#e6f1ff",
+        lightTertiaryBackground: "#8892b0",
+        lightHighlight: "#233554",
+      },
     },
   },
   plugins: [],
 };
+// a8b2d1
