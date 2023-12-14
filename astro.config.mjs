17b3bd1d57e7e7a21ef3412e730e05bef75e2b2d
Bret <bret_gomes811@hotmail.co.uk>
2023-12-14 15:48:25 +0000

    add interactivity for mobile burger menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sentry(), spotlightjs()],
   site: 'https://RGBlife.github.io',
-  base: '/',
+  base: '/'
 });
---
+++ src/components/MenuToggle.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+interface MenuToggleProps {
+  children: React.ReactNode;
+}
+
+const MenuToggle: React.FC<MenuToggleProps> = ({ children }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  return (
+    <>
+      <div className="md:hidden flex justify-end">
+        <button id="menuButton" className="text-white" onClick={toggleMenu}>
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div id="mobileMenu" className={menuVisible ? "" : "hidden"}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default MenuToggle;
