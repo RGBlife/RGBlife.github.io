6dfde5dd226fde4f2382dea30c29536a59d762fe
Bret <bret_gomes811@hotmail.co.uk>
2023-12-16 16:28:32 +0000

    add styles file and more styling done

--- src/components/MenuToggle.tsx
+++ src/components/MenuToggle.tsx
@@ -24,7 +24,7 @@ const MenuToggle: React.FC<MenuToggleProps> = ({ children }) => {
           >
             <path
               stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinejoin="round"
               stroke-width="2"
               d="M4 6h16M4 12h16M4 18h16"
             ></path>
