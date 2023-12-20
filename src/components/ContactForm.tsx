72b4c1346508384009264dc8cb2a1ea450b09f1c
Bret <bret_gomes811@hotmail.co.uk>
2023-12-20 14:26:15 +0000

    added wording for contactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -11,28 +11,55 @@ export const ContactForm = () => {
   };
 
   return (
-    <form className="flex flex-col justify-center items-center " onSubmit={handleSubmit}>
-      <label className="text-darkSecondaryText text-lg mt-5 font-light" htmlFor="name">Name:</label>
+    <form
+      className="flex flex-col justify-center items-center "
+      onSubmit={handleSubmit}
+    >
+      <p>
+        Have a question or want to work together? Leave your details and I'll
+        get back to you as soon as possible.
+      </p>
+      <label
+        className="text-darkSecondaryText text-lg mt-5 font-light"
+        htmlFor="name"
+      >
+        Name:
+      </label>
       <input
         type="text"
         id="name"
         value={name}
         onChange={(event) => setName(event.target.value)}
       />
-      <label className="text-darkSecondaryText text-lg mt-5 font-light" htmlFor="email">Email:</label>
+      <label
+        className="text-darkSecondaryText text-lg mt-5 font-light"
+        htmlFor="email"
+      >
+        Email:
+      </label>
       <input
         type="email"
         id="email"
         value={email}
         onChange={(event) => setEmail(event.target.value)}
       />
-      <label className="text-darkSecondaryText text-lg mt-5 font-light" htmlFor="message">Message:</label>
+      <label
+        className="text-darkSecondaryText text-lg mt-5 font-light"
+        htmlFor="message"
+      >
+        Message:
+      </label>
       <textarea
         id="message"
         value={message}
         onChange={(event) => setMessage(event.target.value)}
       />
-      <button className="text-darkSecondaryText text-lg mt-5 font-light" type="submit">Submit</button>
+      <button
+        className="text-darkSecondaryText text-lg mt-5 font-light"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
   );
 };
