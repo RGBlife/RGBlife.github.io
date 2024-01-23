153c9daf01568b94c5452d53e71fb4d4de8ba861
Bret <bret_gomes811@hotmail.co.uk>
2024-01-23 15:57:15 +0000

    tooltip update

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -8,7 +8,7 @@ export const Tooltip = (props: Props) => {
   return (
     <div className="relative group">
       {children}
-      <span className="absolute hidden group-hover:block bg-darkAccent text-darkBackground p-2 rounded-md text-sm z-10">
+      <span className="absolute opacity-0 group-hover:opacity-100 bg-darkAccent text-darkBackground p-2 rounded-md text-sm z-10 transition-opacity duration-300 select-none pointer-events-none">
         {text}
       </span>
     </div>
