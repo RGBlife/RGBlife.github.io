6afeeb056646618fe3195f7e0a033dc6195a3dd7
Bret <bret_gomes811@hotmail.co.uk>
2024-01-23 15:22:53 +0000

    footer too much padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex bg-darkBackground text-lightPrimaryText py-8 justify-center items-center">
+    <footer className="flex bg-darkBackground text-lightPrimaryText pt-8 pb-1 justify-center items-center">
       <p className="text-sm text-center md:text-left">Created using the Astro framework, TailwindCSS & TypeScript.</p>
     </footer>
   );
