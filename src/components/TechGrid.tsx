bf763a620f9b566f959d3f23b76b8b65d940120f
Bret <bret_gomes811@hotmail.co.uk>
2024-01-24 16:14:36 +0000

    cleanup, move images from public folder

--- src/components/TechGrid.tsx
+++ src/components/TechGrid.tsx
@@ -3,49 +3,49 @@ import { Tooltip } from "./Tooltip";
 export const TechGrid = () => {
   const techs = [
     {
-      icon: "/HTML5_logo.png",
+      icon: "src/assets/HTML5_logo.png",
       name: "HTML",
       description:
         "Learned from an early stage. I always strive to keep HTML as semantic as possible.",
     },
     {
-      icon: "/CSS3_logo.svg",
+      icon: "src/assets/CSS3_logo.svg",
       name: "CSS",
       description:
         "Grew alongside my HTML skills. Evolved to using Tailwind, my go-to CSS framework now.",
     },
     {
-      icon: "/JavaScript_logo.svg",
+      icon: "src/assets/JavaScript_logo.svg",
       name: "JavaScript",
       description:
         "My first programming language, picked up in my Junior CRM Developer role and refined during the Northcoders Bootcamp.",
     },
     {
-      icon: "/Typescript_logo_2020.svg",
+      icon: "src/assets/Typescript_logo_2020.svg",
       name: "TypeScript",
       description:
         "Advanced my JavaScript skills by embracing TypeScript for its robust typing and scalability, especially during the Northcoders Bootcamp.",
     },
     {
-      icon: "/React-icon.svg",
+      icon: "src/assets/React-icon.svg",
       name: "React",
       description:
         "Started using React for side projects and later extensively during the Northcoders Bootcamp. It's key to my front-end development work.",
     },
     {
-      icon: "/Node.js_logo.svg",
+      icon: "src/assets/Node.js_logo.svg",
       name: "Node.js",
       description:
         "Gained proficiency in Node.js during the Northcoders Bootcamp, focusing on building efficient back-end services.",
     },
     {
-      icon: "/expressjs-icon.svg",
+      icon: "src/assets/expressjs-icon.svg",
       name: "Express.js",
       description:
         "Learned as part of server-side development with Node.js during the Northcoders Bootcamp.",
     },
     {
-      icon: "/jestjsio-icon.svg",
+      icon: "src/assets/jestjsio-icon.svg",
       name: "Jest",
       description:
         "Acquired skills in writing tests using Jest wih the Test Driven Development (TDD) approach at the Northcoders Bootcamp, enhancing my ability to write more reliable code.",
