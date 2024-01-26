d3d2dcc98d5beb51121ce48f4c825efffe6fe916
Bret <bret_gomes811@hotmail.co.uk>
2024-01-26 15:20:24 +0000

    forgot to re-add animation to new hamburger menu

--- src/components/MenuToggle.tsx
+++ src/components/MenuToggle.tsx
@@ -24,7 +24,7 @@ export const MenuToggle = () => {
   return (
     <>
       <div className="md:hidden h-14 flex justify-end">
-        <label className="hamburger-menu md:hidden ">
+        <label className="hamburger-menu md:hidden">
           <input type="checkbox" id="menuButton" onClick={toggleMenu} />
         </label>
       </div>
