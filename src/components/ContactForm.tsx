b7162e070a6e627fe7cc45afb088826049750848
Bret <bret_gomes811@hotmail.co.uk>
2024-02-12 20:07:26 +0000

    update header style

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -77,7 +77,7 @@ export const ContactForm = () => {
       className="flex flex-col justify-center items-center relative gap-4 fade-effect"
       onSubmit={handleSubmit}
     >
-          <h1 className="relative self-center text-2xl md:text-4xl font-bold text-darkSecondaryText before:content-[' '] before:absolute before:bg-darkAccent before:w-1/2 before:h-[5px] before:right-0 before:bottom-0 before:-z-10 fade-effect">
+          <h1 className="relative self-center text-2xl md:text-4xl font-bold text-darkSecondaryText before:content-[' '] before:absolute before:bg-darkAccent before:w-full before:-mb-1 before:h-[5px] before:right-0 before:bottom-0 before:-z-10 fade-effect">
       Contact
     </h1>
 
