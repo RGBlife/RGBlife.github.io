c67ed9b0876921610ad14c6cd62472e4eb54e845
Bret <bret_gomes811@hotmail.co.uk>
2024-02-25 20:58:37 +0000

    update v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({ applyBaseStyles: false })],
-  site: "https://RGBlife.github.io",
+  site: "https://bretgomes.dev",
   base: "/",
   output: "server",
   adapter: vercel({
