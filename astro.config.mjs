28eed46dd070289dd9d2e8367cbc7d79042011c5
Bret <bret_gomes811@hotmail.co.uk>
2024-02-25 21:35:12 +0000

    vercel update v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
   site: "https://bretgomes.dev",
   base: "/",
-  output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  output: "server",
+  adapter: vercel()
 });
